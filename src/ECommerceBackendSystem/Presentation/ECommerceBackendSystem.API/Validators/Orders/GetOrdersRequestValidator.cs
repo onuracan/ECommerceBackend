b786d4ce2d12cae568b5f465e7d0d101e734d77b
onuracan <onur.acan@hotmail.com>
2025-05-28 13:47:49 +0300

    .

--- src/ECommerceBackendSystem/Presentation/ECommerceBackendSystem.API/Validators/Orders/GetOrdersRequestValidator.cs
+++ src/ECommerceBackendSystem/Presentation/ECommerceBackendSystem.API/Validators/Orders/GetOrdersRequestValidator.cs
@@ -7,7 +7,6 @@ public class GetTokenRequestValidator : AbstractValidator<GetOrdersRequest>
 {
     public GetTokenRequestValidator()
     {
-        RuleFor(x => x.UserId).NotEmpty().WithMessage("Enter the user id.")
-                              .Must(id => Guid.TryParse(id, out _)).WithMessage("User ID must be a valid GUID.");
+        RuleFor(x => x.UserId).NotEmpty().WithMessage("Enter the user id.");
     }
 }
